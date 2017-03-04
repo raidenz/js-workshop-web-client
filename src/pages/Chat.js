677ab971803eb45477981ef878279faa5bc36775
eezhal92 <arkinthesky.69@gmail.com>
2017-03-04 20:18:27 +0700

    remove routing

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import ChatWindow from '../components/ChatWindow';
 import Navbar from '../components/Navbar';
-import auth from '../lib/auth';
 
 class Chat extends Component {
   onLogOut() {
-    auth.logOut().then(() => {
-      // redirect to login page
-      this.props.router.push('/login');
-    });
+    alert('Your going to log out');
   }
 
   render() {
     return (
       <div className="chat-page">
-        <Navbar name={auth.getUser().name} onLogOut={() => this.onLogOut()} />
+        <Navbar name="John Doe" onLogOut={() => this.onLogOut()} />
         <div className="page page--gray">
           <div className="container">
             <ChatWindow />
